e85ce2a81558954985e395573f1da34e07e510bb
Lorenzo Fiorini <lorenfiorini@live.com>
2022-11-22 14:24:41 +0100

    Function I'll need

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,24 @@
 /*   By: lfiorini <lfiorini@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 13:00:50 by lfiorini          #+#    #+#             */
-/*   Updated: 2022/11/22 13:00:51 by lfiorini         ###   ########.fr       */
+/*   Updated: 2022/11/22 14:24:13 by lfiorini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+
+#endif
